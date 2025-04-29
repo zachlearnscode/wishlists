ba9f53ce926712e5a9b5bbe183dc48b8dbf66e7d
Zachary McNulty <zach@Mac.attlocal.net>
2025-04-29 10:48:11 -0500

    implementing sign in

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import Root from './views/Root.vue'
+import SignIn from './views/SignIn.vue'
 import Dashboard from './views/Dashboard.vue'
 import Wishlist from './views/Wishlist.vue'
 
 const routes = [
   { path: '/', component: Root },
+  { path: '/sign-in', component: SignIn },
   { path: '/dashboard', component: Dashboard },
   { path: '/wishlist/:id', component: Wishlist, props: true },
 ]
